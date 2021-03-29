4e173e8b50c836d4bd06822961abd429b402fa22
Michael Jan <michaeljancontact@gmail.com>
2021-03-29 13:56:55 -0400

    stuf

--- entry.sh
+++ entry.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 repo_path=$1
-echo Repo path is: $repo_path
+echo Path to repo is: $repo_path
 echo It contains:
 ls $repo_path
 
 ssh_command='ssh michael@1.2.3.4'
-echo SSH into the repo using this command:
+echo SSH into the VM using this command:
 echo $ssh_command
