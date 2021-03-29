eecd34104f27fe19c441c42c8f9fb8a5d079235b
Michael Jan <michaeljancontact@gmail.com>
2021-03-29 13:50:36 -0400

    changes

--- entry.sh
+++ entry.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 repo_path=$1
-echo Repo path is: $1
+echo Repo path is: $repo_path
+echo It contains:
+ls $repo_path
 
 ssh_command='ssh michael@1.2.3.4'
 echo "::set-output name=ssh-command::$ssh_command"
